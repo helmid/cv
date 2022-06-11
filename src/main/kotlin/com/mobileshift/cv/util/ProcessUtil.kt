837e6bc5b913b5f2f437d4b26873c9511b8b8eb7
helmid <github@mobile-shift.contractors>
2022-06-11 17:45:56 +0200

    added logging for external process commands and results

--- src/main/kotlin/com/mobileshift/cv/util/ProcessUtil.kt
+++ src/main/kotlin/com/mobileshift/cv/util/ProcessUtil.kt
@@ -1,20 +1,28 @@
 package com.mobileshift.cv.util
 
+import org.slf4j.LoggerFactory
 import java.io.File
 import java.util.concurrent.TimeUnit
 
 object ProcessUtil {
+    private val logger = LoggerFactory.getLogger(ProcessUtil::class.java)
+
     fun runCommand(
         command: String,
         workingDir: File,
         timeoutAmount: Long = 60,
         timeoutUnit: TimeUnit = TimeUnit.SECONDS
-    ): String? = runCatching {
-        ProcessBuilder("\\s".toRegex().split(command))
-            .directory(workingDir)
-            .redirectOutput(ProcessBuilder.Redirect.PIPE)
-            .redirectError(ProcessBuilder.Redirect.PIPE)
-            .start().also { it.waitFor(timeoutAmount, timeoutUnit) }
-            .inputStream.bufferedReader().readText()
-    }.onFailure { it.printStackTrace() }.getOrNull()
+    ): String? {
+        logger.info("Running command $command in ${workingDir.absolutePath}")
+        val result = runCatching {
+            ProcessBuilder("\\s".toRegex().split(command))
+                .directory(workingDir)
+                .redirectOutput(ProcessBuilder.Redirect.PIPE)
+                .redirectError(ProcessBuilder.Redirect.PIPE)
+                .start().also { it.waitFor(timeoutAmount, timeoutUnit) }
+                .inputStream.bufferedReader().readText()
+        }.onFailure { it.printStackTrace() }.getOrNull()
+        logger.info(result)
+        return result
+    }
 }
