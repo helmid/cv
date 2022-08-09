cf6d708d10a0273f89220b1c7cb3b563bf02421c
helmid <github@mobile-shift.contractors>
2022-08-09 21:11:42 +0200

    add image to tex file; fix mail error handling and umlaut issue

--- src/main/kotlin/group/helmi/cv/service/MailServiceImpl.kt
+++ src/main/kotlin/group/helmi/cv/service/MailServiceImpl.kt
@@ -6,19 +6,25 @@ import group.helmi.cv.dto.ContactRequestDTO
 import group.helmi.cv.dto.RequestMailDTO
 import group.helmi.cv.util.CvPathUtil.getContactFormMailJson
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.mail.MailException
 import org.springframework.mail.SimpleMailMessage
 import org.springframework.mail.javamail.JavaMailSender
 import org.springframework.stereotype.Service
 
 @Service
 class MailServiceImpl(@Autowired private val javaMailSender: JavaMailSender) : MailService {
-    override fun sendContactRequestMail(contactRequestDTO: ContactRequestDTO) {
+    override fun sendContactRequestMail(contactRequestDTO: ContactRequestDTO): Boolean {
         val msg = SimpleMailMessage()
         val mailBaseData = jacksonObjectMapper().readValue<RequestMailDTO>(getContactFormMailJson())
         msg.setFrom(mailBaseData.from)
         msg.setTo(mailBaseData.to)
         msg.setSubject("${mailBaseData.subject}${contactRequestDTO.email}")
         msg.setText("${mailBaseData.text}${contactRequestDTO.toMailDetails()}")
-        javaMailSender.send(msg)
+        return try {
+            javaMailSender.send(msg)
+            true
+        } catch (e: MailException) {
+            false
+        }
     }
 }
