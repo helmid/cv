3fa6dfe9219539f14f16554cdcbe38ee59cd7a38
helmid <github@mobile-shift.contractors>
2023-12-28 17:12:37 +0100

    bump versions

--- buildSrc/src/main/kotlin/Version.kt
+++ buildSrc/src/main/kotlin/Version.kt
@@ -1,14 +1,14 @@
 object Version {
     object Plugin {
         object Spring {
-            const val boot = "2.7.11"
-            const val dependencyManagement = "1.1.0"
+            const val boot = "2.7.18"
+            const val dependencyManagement = "1.1.4"
         }
 
         object Kotlin {
-            const val jvm = "1.8.21"
-            const val spring = "1.8.21"
-            const val serialization = "1.8.21"
+            const val jvm = "1.9.22"
+            const val spring = "1.9.22"
+            const val serialization = "1.9.22"
         }
     }
 }
